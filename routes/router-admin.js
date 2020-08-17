8de58d04915bb38d9a035e6b4e0e9daa57e8a318
Dimantik <sluhdimaa@gmail.com>
2020-08-17 05:15:07 +0300

    Commit

--- routes/router-admin.js
+++ routes/router-admin.js
@@ -10,8 +10,17 @@ router.get('/signin', (req, res) => {
     // } else {
     //     res.status(401)
     // }
-    res.status(200)
-    res.send({ "email": "Dimantik", "password": "201031" })
+    console.log("Try login " + req.query.email + " " + req.query.password)
+    if (req.query.email == "Dimantik" && req.query.password == "201031") {
+        console.log("Success")
+        res.status(200)
+        res.send({ "email": "Dimantik", "password": "201031" })
+    } else {
+        console.log("Failure")
+        res.status(401)
+        res.send();
+    }
+    
 })
 
 module.exports = router;
